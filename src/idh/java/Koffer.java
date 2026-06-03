7c7c7788b27e8175899fcc1b9cac153ccb5c4434
amalorod <alicmalorodow@gmail.com>
2026-06-03 15:15:16 +0200

    Lösung Aufgabe 1

--- src/idh/java/Koffer.java
+++ src/idh/java/Koffer.java
@@ -2,6 +2,21 @@ package idh.java;
 
 public class Koffer {
 	public static void main(String[] args) {
-		System.out.println("Ich packe in meinen Koffer ein Modulhandbuch und Fleisch.");
-	}
-}
+			System.out.println("Ich packe in meinen Koffer ein Modulhandbuch, Fleisch und ein Alpaka:\n"
+						+ "    _    _\n"
+						+ "   (o\\_/_o)\n"
+						+ "   ( . . )\n"
+						+ "    ( v )\n"
+						+ "     | |\n"
+						+ "    /   \\\n"
+						+ "   /     \\________\n"
+						+ "  |               \\\n"
+						+ "  |   ██████████   |\n"
+						+ "  |   ██████████   |\n"
+						+ "   \\_            _/\n"
+						+ "     | |      | |\n"
+						+ "     | |      | |\n"
+						+ "    (___)    (___)");
+			}
+		}
+
