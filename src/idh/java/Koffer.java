8cb42ae51531763fb2ec2b1550c423088affee04
Knurhagen <knurhagen@yahoo.com>
2026-06-04 13:37:10 +0200

    Plus LP "Made in Japan" (Deep Purple 1972)

--- src/idh/java/Koffer.java
+++ src/idh/java/Koffer.java
@@ -2,6 +2,6 @@ package idh.java;
 
 public class Koffer {
 	public static void main(String[] args) {
-		System.out.println("Ich packe in meinen Koffer ein Modulhandbuch, Fleisch und die LP "Made in Japan");
+		System.out.println("Ich packe in meinen Koffer ein Modulhandbuch, Fleisch und die LP \"Made in Japan\" ");
 	}
 }
