1697fe3c9e00c911921537d82908aee1b0a36dc2
Fabian Radlow <fabianradlow@gmail.com>
2026-06-05 18:29:01 +0200

    b

--- src/idh/java/ATM.java
+++ src/idh/java/ATM.java
@@ -36,7 +36,7 @@ public class ATM  {
 	public void cashout(int amount) {
 		// check for cash in the ATM
 		if (amount > cash) {
-			System.out.println("Sorry, not enough cash left, broke boy");
+			System.out.println("Sorry, not enough cash left, broke b boy");
 			return;
 		}
 		
