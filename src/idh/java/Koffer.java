ab814b7e5caf34a08f53ee10dc9e50dcd448857a
LodeRunner83 <276631240+LodeRunner83@users.noreply.github.com>
2026-06-07 17:24:08 +0200

    Partially merged with Branch zoezd29 (Commit: ee0c8e6) ... in order to re cover what was lost by brute git bash force

--- src/idh/java/Koffer.java
+++ src/idh/java/Koffer.java
@@ -17,11 +17,15 @@ public class Koffer {
 						+ "     | |      | |\n"
 						+ "     | |      | |\n"
 						+ "    (___)    (___)\n"
-						+ ""
-						+ "maybe a Leo, but sure no Lion\n"  
-						+ "die LP \"Made in Japan\"\n"
+						+ "\n"
+						+ "maybe a Leo, but sure no Lion\n"
+						+ "die LP \"Made in Japan\"\n" 
 						+ "Alpakageschirr\n" 
-						+ "Luhmanns Zettelkasten und einen Apple IIe\n"
-						+ "ein gutes Buch");
+						+ "Luhmanns Zettelkasten und einen Apple IIe\n" 
+						+ "Nils:)\n"
+						+ "das Bundesverfassungsgericht (Man weiß ja nie)\n"
+						+ "ein Basketball\n" 
+						+ "einen Schokokeks\n");
+
 			}
 		}
