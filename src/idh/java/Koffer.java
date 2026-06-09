3aa675930d9eb2b91c6d4ae8b0931b3d6a484887
Hannah Frerich <hfauss@proton.me>
2026-06-09 10:08:53 +0200

    Wackersteine ergänzt

--- src/idh/java/Koffer.java
+++ src/idh/java/Koffer.java
@@ -30,12 +30,13 @@ public class Koffer {
 						+ "einen Nintendo 3DS mit Pokemon Mond\n"
 						+ "einen Tiger mit 11 Streifen\n"
 						+ "meinen halben Kleiderschrank\n"
-						+ " eine Zahnbürste und Zahnpasta\n"
+						+ "eine Zahnbürste und Zahnpasta\n"
 						+ "ein zweites Alpaka damit das andere nicht einsam ist\n"
 			            + "ein drittes Alpaka\n"
 						+ "Fortnite\n"
 			            + "Kpop-Album"
-			            + "einen weiteren Koffer\n");
+			            + "einen weiteren Koffer\n"
+						+ "Wackersteine\n");
 
 			}
 		}
