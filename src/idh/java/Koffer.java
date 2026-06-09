37a08eb32fad9144c85af88024e28f7161a5d2f6
Johanna Faßl <Johanna Fal@PRO-NB-004.fritz.box>
2026-06-09 22:01:47 +0200

    Milchscnitte

--- src/idh/java/Koffer.java
+++ src/idh/java/Koffer.java
@@ -3,5 +3,6 @@ package idh.java;
 public class Koffer {
 	public static void main(String[] args) {
 		System.out.println("Ich packe in meinen Koffer ein Modulhandbuch.");
+		System.out.print("Ich packe in meinen Koffer ein Modulhandbuch und eine Milchschnitte");
 	}
 }
